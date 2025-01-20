78c03eb119daafe26be6a1ddbf67be4991ed0eaa
frankz61 <15981955337@163.com>
2025-01-20 16:51:11 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  ["docker.frankzhangs.top"]: dockerHub,
+  ["quay.frankzhangs.top"]: "https://quay.io",
+  ["gcr.frankzhangs.top"]: "https://gcr.io",
+  ["k8s-gcr.frankzhangs.top"]: "https://k8s.gcr.io",
+  ["k8s.frankzhangs.top"]: "https://registry.k8s.io",
+  ["ghcr.frankzhangs.top"]: "https://ghcr.io",
+  ["cloudsmith.frankzhangs.top"]: "https://docker.cloudsmith.io",
+  ["ecr.frankzhangs.top"]: "https://public.ecr.aws",
 
   // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  ["docker-staging.frankzhangs.top"]: dockerHub,
 };
 
 function routeByHosts(host) {
